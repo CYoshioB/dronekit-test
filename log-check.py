9f8a5ddc1488abfe7a99c35d958a067842515a43
CYoshioB <Colin.Bender@student.csulb.edu>
2024-06-02 17:40:11 -0700

    Reset to dummy value version

--- log-check.py
+++ log-check.py
@@ -1,6 +1,6 @@
 from dronekit import connect, VehicleMode, LocationGlobal
 from pymavlink import mavutil
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO
 import time
 from datetime import datetime
 import argparse
@@ -14,39 +14,10 @@ print('Connecting to vehicle on: %s' % args.connect)
 vehicle = connect(args.connect, baud=57600, wait_ready=True)
 #921600 is the baudrate that you have set in the mission plannar or qgc
 
-# Function to arm and then takeoff to a user specified altitude
-def arm_and_takeoff(aTargetAltitude):
-
-  print("Basic pre-arm checks")
-  # Don't let the user try to arm until autopilot is ready
-  while not vehicle.is_armable:
-    print(" Waiting for vehicle to initialise...")
-    time.sleep(1)
-        
-  print("Arming motors")
-  # Copter should arm in GUIDED mode
-  vehicle.mode    = VehicleMode("GUIDED")
-  vehicle.armed   = True
-
-  while not vehicle.armed:
-    print(" Waiting for arming...")
-    time.sleep(1)
-
-  print("Taking off!")
-  vehicle.simple_takeoff(aTargetAltitude) # Take off to target altitude
-
-  # Check that vehicle has reached takeoff altitude
-  while True:
-    print(" Altitude: ", vehicle.location.global_relative_frame.alt )
-    #Break and return from function just below target altitude.        
-    if vehicle.location.global_relative_frame.alt>=aTargetAltitude*0.95: 
-      print("Reached target altitude")
-      break
-    time.sleep(1)
-
 # Global values to be updated everytime callback function is invoked
 global id, iso_date, lat, lon
 
+"""
 def location_callback(self, attr_name, newVal):
     # Logic to check which ID is recognized
     if GPIO.input(1) == 1 and GPIO.input(2) == 0:
@@ -64,9 +35,8 @@ def location_callback(self, attr_name, newVal):
     print("CSULB_UAV_WaterBlast!_%d_%sZ_%.6f_%.6f" % (id, iso_date, lat, lon))
 
 vehicle.add_attribute_listener('location.global_frame', location_callback)
-
-
 """
+
 # Dummy Values
 id = 23
 iso_date = datetime.now().isoformat()
@@ -75,4 +45,3 @@ lon = 125.1029384756
 
 print("RTXDC_24")
 print("CSULB_UAV_WaterBlast!_%d_%sZ_%.6f_%.6f" % (id, iso_date, lat, lon))
-"""
