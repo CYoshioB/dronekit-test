56f7f7976478356a8cfeb9b2602f8184ff8f53d5
CYoshioB <Colin.Bender@student.csulb.edu>
2024-06-02 17:27:07 -0700

    Elaborated callback routine for water blast logging. Will add to full routine for Challenge 1.

--- log-check.py
+++ log-check.py
@@ -1,10 +1,10 @@
 from dronekit import connect, VehicleMode, LocationGlobal
 from pymavlink import mavutil
-# import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 import time
 from datetime import datetime
 import argparse
-"""
+
 parser = argparse.ArgumentParser()
 parser.add_argument('--connect', default='127.0.0.1:14550')
 args = parser.parse_args()
@@ -43,8 +43,30 @@ def arm_and_takeoff(aTargetAltitude):
       print("Reached target altitude")
       break
     time.sleep(1)
-"""
 
+# Global values to be updated everytime callback function is invoked
+global id, iso_date, lat, lon
+
+def location_callback(self, attr_name, newVal):
+    # Logic to check which ID is recognized
+    if GPIO.input(1) == 1 and GPIO.input(2) == 0:
+        id = 22
+    elif GPIO.input(2) == 1 and GPIO.input(1) == 0:
+        id = 23
+    else:
+        return  # If no Aruco is recognized, break out of the callback
+    # Update current time and coordinates after Aruco is recognized
+    iso_date = datetime.now().isoformat()
+    lat = newVal.lat
+    lon = newVal.lon
+    # Print out the updated values
+    print("RTXDC_24")
+    print("CSULB_UAV_WaterBlast!_%d_%sZ_%.6f_%.6f" % (id, iso_date, lat, lon))
+
+vehicle.add_attribute_listener('location.global_frame', location_callback)
+
+
+"""
 # Dummy Values
 id = 23
 iso_date = datetime.now().isoformat()
@@ -53,3 +75,4 @@ lon = 125.1029384756
 
 print("RTXDC_24")
 print("CSULB_UAV_WaterBlast!_%d_%sZ_%.6f_%.6f" % (id, iso_date, lat, lon))
+"""
